fdeaca62eb911abd7e1211e729ea0c7289dac77b
kirillIsGoodCoder <36628319+kirillIsGoodCoder@users.noreply.github.com>
2018-09-28 10:38:43 +0400

    unstable version

---
+++ src/Director.java
@@ -0,0 +1,15 @@
+public class Director extends Person{
+
+    private int id;
+    private boolean[][] workPlan;
+
+    public Director (int id) {
+        this.id = id;
+
+    }
+
+    public int getId(){
+        return id;
+    }
+
+}
